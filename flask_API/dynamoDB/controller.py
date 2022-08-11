01f409f1f736d27517cee21554b127f2cb981ff5
s2482266 <d.d.tran@student.utwente.nl>
2022-08-11 23:31:24 +0700

    add api for get all recipes and add recipes, response nutrition score

--- flask_API/dynamoDB/controller.py
+++ flask_API/dynamoDB/controller.py
@@ -1,7 +1,11 @@
 from inspect import Attribute
+from urllib import response
 from boto3 import client, resource
+from datetime import datetime
+from decimal import Decimal
 
 import dynamoDB.config as config
+import json
 
 # import config
 
@@ -28,10 +32,34 @@ resource = resource(
 )
 
 
-def create_table_foodList():
+# def create_table_foodList():
+#     # try:
+#     table = resource.create_table(
+#         TableName="Foods",  # Name of the table
+#         KeySchema=[
+#             {
+#                 "AttributeName": "id",
+#                 "KeyType": "HASH",  # RANGE = sort key, HASH = partition key
+#             }
+#         ],
+#         AttributeDefinitions=[
+#             {
+#                 "AttributeName": "id",  # Name of the attribute
+#                 "AttributeType": "N",  # N = Number (B= Binary, S = String)
+#             }
+#         ],
+#         ProvisionedThroughput={"ReadCapacityUnits": 10, "WriteCapacityUnits": 10},
+#     )
+#     # except resource.exceptions.ResourceInUseException:
+#     #     # return "Table already exists"
+#     #     pass
+#     return table
+
+
+def createRecipesTable():
     # try:
     table = resource.create_table(
-        TableName="Foods",  # Name of the table
+        TableName="Recipes",  # Name of the table
         KeySchema=[
             {
                 "AttributeName": "id",
@@ -55,71 +83,85 @@ def create_table_foodList():
 # create_table_movie()
 
 # create_table_foodList()
-FoodListTable = resource.Table("Foods")
+# FoodListTable = resource.Table("Foods")
+RecipesTable = resource.Table("Recipes")
 
 
-def write_to_foodList(id, ingredients, description):
-    print(id, ingredients, description)
-    print("------------")
-    response = FoodListTable.put_item(
-        Item={"id": id, "ingredients": ingredients, "description": description}
+def addRecipe(data, nutriScore):
+    data = json.loads(json.dumps(data), parse_float=Decimal)
+    nutriScore = Decimal(nutriScore)
+    hashedId = hash(data["cookingMethod"] + data["mealType"])
+    now = datetime.now()
+    stringifiedTime = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+    response = RecipesTable.put_item(
+        Item={
+            "id": hashedId,
+            "data": data,
+            "nutriScore": nutriScore,
+            "date": stringifiedTime,
+        }
     )
+
     print(response)
     return response
 
 
-# write_to_foodList(
-#     1,
-#     [{"name": "beef", "amount": "100g"}, {"name": "pork", "amount": "200g"}],
-#     "duc's food",
-# )
+def getAllRecipes():
+    response = RecipesTable.scan()
+    data = response["Items"]
+    while "LastEvaluatedKey" in response:
+        print(response["LastEvaluatedKey"])
+        response = RecipesTable.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
+        data.extend(response["Items"])
+    return response
 
 
 # def get_all_items():
 #     return FoodListTable
 
 
-def get_food_from_foodList(id):
-    # print(FoodListTable)
-    response = FoodListTable.get_item(
-        Key={
-            "id": id,
-        },
-    )
-    print(response)
-    return response["Item"]
+# def get_food_from_foodList(id):
+#     # print(FoodListTable)
+#     response = FoodListTable.get_item(
+#         Key={
+#             "id": id,
+#         },
+#     )
+#     print(response)
+#     return response["Item"]
 
 
-# get_food_from_foodList(1)
+# # get_food_from_foodList(1)
 
 
-def update_food(id, data: dict):
-    ingredients = data["ingredients"]
-    description = data["description"]
-    response = FoodListTable.update_item(
-        Key={
-            "id": id,
-        },
-        ConditionExpression="attribute_exists(id)",
-        UpdateExpression="SET ingredients = :val1, description = :val2",
-        ReturnValues="UPDATED_NEW",
-        ExpressionAttributeValues={":val1": ingredients, ":val2": description},
-    )
+# def update_food(id, data: dict):
+#     ingredients = data["ingredients"]
+#     description = data["description"]
+#     response = FoodListTable.update_item(
+#         Key={
+#             "id": id,
+#         },
+#         ConditionExpression="attribute_exists(id)",
+#         UpdateExpression="SET ingredients = :val1, description = :val2",
+#         ReturnValues="UPDATED_NEW",
+#         ExpressionAttributeValues={":val1": ingredients, ":val2": description},
+#     )
 
-    return response
-
-
-# update_food(
-#     1,
-#     {
-#         "ingredients": [
-#             {"name": "beef", "amount": "100g"},
-#             {"name": "kool", "amount": "200g"},
-#         ],
-#         "description": "vkl",
-#     },
-# )
+#     return response
 
 
-def delete_food_from_db(id):
-    response = FoodListTable.delete_item(Key={"id": id})
+# # update_food(
+# #     1,
+# #     {
+# #         "ingredients": [
+# #             {"name": "beef", "amount": "100g"},
+# #             {"name": "kool", "amount": "200g"},
+# #         ],
+# #         "description": "vkl",
+# #     },
+# # )
+
+
+# def delete_food_from_db(id):
+#     response = FoodListTable.delete_item(Key={"id": id})
--- flask_API/main.py
+++ flask_API/main.py
@@ -1,9 +1,14 @@
+from decimal import Decimal
 from urllib import response
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 import dynamoDB.controller as dynamodb
+from lib.nutritionCalculator import calculateScore
+
+# import json
+# from decimal import Decimal
 
 # Init app
 app = Flask(__name__)
@@ -35,7 +40,8 @@ class Food:
 @app.route("/", methods=["GET"])
 def root_route():
     try:
-        dynamodb.create_table_foodList()
+        # dynamodb.create_table_foodList()
+        dynamodb.createRecipesTable()
     except Exception:
         return "Table already exists"
         # pass
@@ -61,6 +67,7 @@ def add_food():
         if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
             return {
                 "msg": "Add food successful",
+                # "nutritionScore": Decimal()
             }
         return {"msg": "error occurred", "response": response}
     # else:
@@ -114,6 +121,52 @@ def updateFood(id):
         }
 
     return {"msg": "Some error occured", "response": response}
+
+
+@app.route("/recipes", methods=["POST", "GET"])
+def addRecipe():
+    if request.method == "POST":
+
+        data = request.get_json()
+        # print(data["id"], data["ingredients"], data["description"])
+
+        totalCalories = 0
+        totalSugar = 0
+        totalSaturatedFat = 0
+        totalFat = 0
+        totalSodium = 0
+        totalProtein = 0
+        totalFiber = 0
+        totalCarb = 0
+        for ingredient in data["ingredients"]:
+            totalCalories += ingredient["nutrition"]["kcal"]["amount"]
+            totalSugar += ingredient["nutrition"]["sugars"]["amount"]
+            totalSaturatedFat += ingredient["nutrition"]["saturates"]["amount"]
+            totalFat += ingredient["nutrition"]["fat"]["amount"]
+            totalSodium += ingredient["nutrition"]["sodium"]["amount"]
+            totalProtein += ingredient["nutrition"]["protein"]["amount"]
+            totalFiber += ingredient["nutrition"]["fibre"]["amount"]
+            totalCarb += ingredient["nutrition"]["carbs"]["amount"]
+        nutritionScore = calculateScore(
+            totalCalories,
+            totalSugar,
+            totalSaturatedFat,
+            totalFat,
+            totalSodium,
+            totalProtein,
+            totalFiber,
+            totalCarb,
+        )
+        # return response
+        # print(data)
+        response = dynamodb.addRecipe(data, nutriScore=nutritionScore)
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            return {"msg": "Add food successful", "nutritionScore": nutritionScore}
+        return {"msg": "error occurred", "response": response}
+        # return {"msg": data}
+    # else:
+    getResponse = dynamodb.getAllRecipes()
+    return getResponse
 
 
 # Run Server
