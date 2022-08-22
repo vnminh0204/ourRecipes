24d0bbe4693cf59798168e328d061ee15d5dc379
vnminh24 <vnminh0204@gmail.com>
2022-08-22 15:41:50 +0200

    change nav bar

--- src/react_client/src/components/navBar.jsx
+++ src/react_client/src/components/navBar.jsx
@@ -3,48 +3,85 @@ import { Link, NavLink } from "react-router-dom";
 import logo from "./logo.png";
 
 const NavBar = () => {
-  const [show,setShow] = useState(false);
+  const [show, setShow] = useState(false);
   return (
-    <nav className="navbar navbar-expand-lg my-navbar">
+    <nav className="my-navbar">
       <Link className="navbar-brand my-brand" to="/">
         <img src={logo} width="70" height="50" alt="OurRecipes" />
       </Link>
-      <div className="navbar-nav">
-        <NavLink className="nav-item nav-link" to="/recipes">
-          Recipes
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/customer">
-          Customer
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/planner">
-          Planner
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/login">
-          Login
-        </NavLink>
-        <NavLink className="nav-item nav-link" to="/register">
-          Register
-        </NavLink>
-      </div>
-      <button
-        className="btn btn-primary"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#collapseExample"
-        aria-expanded="false"
-        aria-controls="collapseExample"
-        onClick = {() => setShow(!show)}
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className= {show? "collapse show" : "collapse" } id="collapseExample">
-        <div className="card card-body">
-          Some placeholder content for the collapse component. This panel is
-          hidden by default but revealed when the user activates the relevant
-          trigger.
+      <ul className="nav-links">
+        <div className="my-menu">
+          <li>
+            <NavLink className="nav-item nav-link" to="/recipes">
+              Recipes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-item nav-link" to="/planner">
+              Planner
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-item nav-link" to="/login">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-item nav-link" to="/register">
+              Register
+            </NavLink>
+          </li>
+          <li className="my-services">
+            <a href="/">Services</a>
+            <ul className="my-dropdown">
+              <li>
+                <a href="/">Dropdown 1 </a>
+              </li>
+              <li>
+                <a href="/">Dropdown 2</a>
+              </li>
+              <li>
+                <a href="/">Dropdown 2</a>
+              </li>
+              <li>
+                <a href="/">Dropdown 3</a>
+              </li>
+              <li>
+                <a href="/">Dropdown 4</a>
+              </li>
+            </ul>
+          </li>
         </div>
-      </div>
+      </ul>
     </nav>
+    // <nav className="navbar navbar-expand-lg my-navbar">
+
+    //   <div className="navbar-nav">
+
+    //     <NavLink className="nav-item nav-link" to="/customer">
+    //       Customer
+    //     </NavLink>
+
+    //   </div>
+    //   <button
+    //     className="btn btn-primary"
+    //     type="button"
+    //     data-bs-toggle="collapse"
+    //     data-bs-target="#collapseExample"
+    //     aria-expanded="false"
+    //     aria-controls="collapseExample"
+    //     onClick = {() => setShow(!show)}
+    //   >
+    //     <span className="navbar-toggler-icon"></span>
+    //   </button>
+    //   <div className= {show? "collapse show" : "collapse" } id="collapseExample">
+    //     <div className="card card-body">
+    //       Some placeholder content for the collapse component. This panel is
+    //       hidden by default but revealed when the user activates the relevant
+    //       trigger.
+    //     </div>
+    //   </div>
+    // </nav>
   );
 };
 
